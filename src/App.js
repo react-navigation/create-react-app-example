a77f5fac775e0d659d16ecc0387898fa75f80554
Eric Vicenti <eric@aven.io>
2018-10-15 15:14:10 -0700

    starting to use react-navigation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-class App extends Component {
+import {
+  createNavigator,
+  SwitchRouter,
+  SceneView
+} from "@react-navigation/core";
+import createBrowserApp from "@react-navigation/web/createBrowserApp";
+
+const Home = () => <div>Home Screen</div>;
+const Profile = () => <div>Profile Screen</div>;
+
+class SidebarView extends React.Component {
   render() {
+    const { descriptors, navigation } = this.props;
+    const activeKey = navigation.state.routes[navigation.state.index].key;
+    const descriptor = descriptors[activeKey];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1> Hello Navigator!</h1>
+        <SceneView
+          navigation={descriptor.navigation}
+          component={descriptor.component}
+        />
       </div>
     );
   }
 }
+
+const AppNavigator = createNavigator(
+  SidebarView,
+  SwitchRouter({
+    Home,
+    Profile
+  })
+);
+
+const App = createBrowserApp(AppNavigator);
 
 export default App;
